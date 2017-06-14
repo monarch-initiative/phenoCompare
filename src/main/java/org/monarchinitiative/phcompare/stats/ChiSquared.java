a2e470685f99d4b21b7e27ca71a960d3af9fd410
pnrobinson <peter.robinson@jax.org>
2017-06-14 18:38:59 -0400

    Empty test classes

---
+++ src/main/java/org/monarchinitiative/phcompare/stats/ChiSquared.java
@@ -0,0 +1,7 @@
+package org.monarchinitiative.phcompare.stats;
+
+/**
+ * Created by robinp on 6/14/17.
+ */
+public class ChiSquared {
+}
---
+++ src/test/java/org/monarchinitiative/phcompare/stats/ChiSquaredTest.java
@@ -0,0 +1,16 @@
+package org.monarchinitiative.phcompare.stats;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by robinp on 6/14/17.
+ */
+public class ChiSquaredTest {
+
+
+    @Test
+    public void testSanity() {
+        Assert.assertEquals(1,1);
+    }
+}
