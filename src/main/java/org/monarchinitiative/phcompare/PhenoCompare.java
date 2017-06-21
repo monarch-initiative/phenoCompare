dfd394d33f07d7bb942928c2b878bc4b04e60128
Hannah Blau <hannah.blau@jax.org>
2017-06-21 19:21:09 -0400

    Expanded class PhenoComare with code to count patients for each node of the ontology. Not yet tested.

--- src/main/java/org/monarchinitiative/phcompare/PhenoCompare.java
+++ src/main/java/org/monarchinitiative/phcompare/PhenoCompare.java
@@ -1,9 +1,6 @@
 package org.monarchinitiative.phcompare;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -12,7 +9,6 @@ import java.util.Map.Entry;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import ontologizer.association.Association;
-import ontologizer.io.annotation.AssociationParser;
 import ontologizer.io.obo.OBOParser;
 import ontologizer.io.obo.OBOParserException;
 import ontologizer.io.obo.OBOParserFileInput;
@@ -31,76 +27,69 @@ import ontologizer.types.ByteString;
 public class PhenoCompare {
     private static final String hpoPath = "/Users/blauh/phenoCompare/hp.obo";
     private static final int NUM_GROUPS = 2;
-    private static final int GROUPA = 0;
-    private static final int GROUPB = 1;
 
-    private static Ontology hpo;
-    private static PatientGroup[] patientGroups = new PatientGroup[NUM_GROUPS];
-    private static Map<TermID, Integer[]> patientCounts = new HashMap<>();
+    private Ontology hpo;
+    private PatientGroup[] patientGroups = new PatientGroup[NUM_GROUPS];
+    private SortedMap<TermID, int[]> patientCounts = new TreeMap<>();
 
-    public static void main(String[] args) {
-        if (args.length != 3) {
-            System.err.println("Error: incorrect number of arguments");
-            System.err.println("Usage: java -jar phenoCompare.jar groupAdirectory groupBdirectory outDirectory");
-            System.exit(1);
-        }
-
-        try {
-            // Load Ontology from file
-            hpo = parseObo(hpoPath);
-        } catch (IOException e) {
-            System.err.println("ERROR: Problem reading OBO file " + hpoPath + "\n\n");
-            e.printStackTrace();
-            System.exit(1);
-        } catch (OBOParserException e) {
-            System.err.println("ERROR: Problem parsing OBO file " + hpoPath + "\n\n");
-            e.printStackTrace();
-            System.exit(1);
-        }
-        // Read patient files for groups A and B
-        try {
-            for (int i = 0; i < NUM_GROUPS; i++) {
-                patientGroups[i] = new PatientGroup(args[i]);
-                patientGroups[i].readPatientFiles();
-            }
-        } catch (IOException e) {
-            System.err.println("ERROR: Problem reading patient files, " + e.getMessage() + "\n\n");
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        // For each node in the HPO ontology that covers one or more patients, count how many patients
-        // in each group fall under that node. Any node of the hierarchy that is not referenced has counts of
-        // 0 for each group.
-        countPatients();
+    public PhenoCompare(String hpoPath) throws IOException, OBOParserException {
+        // Load Ontology from file
+        hpo = parseObo(hpoPath);
     }
 
-    private static void countPatients() {
+    private void createPatientGroups(String[] paths) throws IOException {
+        for (int i = 0; i < NUM_GROUPS; i++) {
+            patientGroups[i] = new PatientGroup(paths[i]);
+            patientGroups[i].readPatientFiles();
+        }
+    }
+
+    private void countPatients() {
         for (int g = 0; g < NUM_GROUPS; g++) {
             for (Patient p : patientGroups[g].getGroupMembers()) {
-                for (TermID tid : p.getHpoTerms()) {
-                    updateCount(tid, g);
-                    for (TermID pid : hpo.getTermParents(tid)) {
-                        updateCount(pid, g);
-                    }
-                }
+                countPatient(p, g);
             }
         }
     }
 
-            /* Load associations
-            List<Association> associations = parseAssociations(args[1], hpo);
+    private void countPatient(Patient p, int group) {
+        for (TermID tid : p.getHpoTerms()) {
+            // getTermsOfInducedGraph returns a set of TermIDs for ancestors of tid
+            for (TermID aid : hpo.getTermsOfInducedGraph(null, tid)) {
+                updateCount(aid, group);
+            }
+        }
+    }
 
-            // Compute information content
-            SortedMap<TermID, Double> informationContent =
-                    computeInformationContent(hpo, associations);
+    private void updateCount(TermID tid, int group) {
+        int[] counts;
 
-            // Write out information content.
-            writeInformationContent(args[2], informationContent);
-            */
+        if (patientCounts.containsKey(tid)) {
+            patientCounts.get(tid)[group]++;
+        }
+        else {
+            counts = new int[NUM_GROUPS];
+            counts[group]++;
+            patientCounts.put(tid, counts);
+        }
+    }
 
-    private static void updateCount(TermID tid, int g) {
-//        if (patientCounts.get(tid))
+    private void displayResults(String outPath) throws IOException {
+        TermID tid;
+        int[] counts;
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(outPath));
+        for (Map.Entry<TermID, int[]> entry : patientCounts.entrySet()) {
+            tid = entry.getKey();
+            counts = entry.getValue();
+
+            bw.write(String.format("%s \t%s", tid, hpo.getTerm(tid).getName().toString()));
+            for (int i = 0; i < NUM_GROUPS; i++) {
+                bw.write(String.format("\t%s%c: %d6", "group", ('A' + i), counts[i]));
+            }
+            bw.newLine();
+        }
+        bw.close();
     }
 
     private static Ontology parseObo(String pathObo) throws IOException, OBOParserException {
@@ -115,21 +104,6 @@ public class PhenoCompare {
         final Ontology ontology = Ontology.create(termContainer);
         System.err.println("=> done reading OBO file");
         return ontology;
-    }
-
-    private static List<Association> parseAssociations(String pathGaf, Ontology ontology)
-            throws IOException {
-        System.err.println("Reading associations from GAF file " + pathGaf + " ...");
-        AssociationParser parser =
-                new AssociationParser(new OBOParserFileInput(pathGaf), ontology.getTermMap());
-        while (!parser.parse())
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                // swallow
-            }
-        System.err.println("=> done reading GAF file");
-        return parser.getAssociations();
     }
 
     private static SortedMap<TermID, Double> computeInformationContent(Ontology ontology,
@@ -172,5 +146,52 @@ public class PhenoCompare {
         }
         out.close();
         System.err.println("=> done writing DOT file");
+    }
+
+    public static void main(String[] args) {
+        PhenoCompare phenoC = null;
+
+        if (args.length != NUM_GROUPS + 1) {
+            System.err.println("Error: incorrect number of arguments");
+            // TODO: fix usage message so it works for any number of groups not just 2
+            System.err.println("Usage: java -jar phenoCompare.jar groupAdirectory groupBdirectory outFile");
+            System.exit(1);
+        }
+
+        // Load Ontology from file
+        try {
+            phenoC = new PhenoCompare(hpoPath);
+        } catch (IOException e) {
+            System.err.println("ERROR: Problem reading OBO file " + hpoPath + "\n\n");
+            e.printStackTrace();
+            System.exit(1);
+        } catch (OBOParserException e) {
+            System.err.println("ERROR: Problem parsing OBO file " + hpoPath + "\n\n");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        // Read patient files and create patient groups.
+        try {
+            phenoC.createPatientGroups(args);
+        } catch (IOException e) {
+            System.err.println("ERROR: Problem reading patient files, " + e.getMessage() + "\n\n");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        // For each node in the HPO ontology that covers one or more patients, count how many patients
+        // in each group fall under that node. Any node of the hierarchy that is not referenced has counts of
+        // 0 for each group.
+        phenoC.countPatients();
+
+        // Display counts for each node of the ontology that has a non-zero count for one or more groups.
+        try {
+            phenoC.displayResults(args[NUM_GROUPS]);
+        } catch (IOException e) {
+            System.err.println("ERROR: Problem writing output file " + args[NUM_GROUPS] + " : " +
+                    e.getMessage() + "\n\n");
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
