f446c9f45512f9a5ce8b6f04da7920ee6a88a754
pnrobinson <peter.robinson@jax.org>
2017-06-22 17:30:58 -0400

    Empty test classes

--- src/main/java/org/monarchinitiative/phcompare/stats/ChiSquared.java
+++ src/main/java/org/monarchinitiative/phcompare/stats/ChiSquared.java
@@ -1,7 +1,44 @@
 package org.monarchinitiative.phcompare.stats;
+import static org.apache.commons.math3.stat.inference.TestUtils.chiSquare;
+import static org.apache.commons.math3.stat.inference.TestUtils.chiSquareTest;
+
 
 /**
  * Created by robinp on 6/14/17.
  */
 public class ChiSquared {
+
+
+    /** Value of observed group 1. */
+    private long o[][];
+    /** Value of observed group 2. */
+    //private double e[];
+
+    private double chisquare;
+    private double chiSquareP;
+
+    public ChiSquared(long[][] observed) {
+        //e=expected;
+        o=observed;
+        calculateChi();
+        calculateChiP();
+    }
+
+    public ChiSquared() {
+
+    }
+
+    private void calculateChi() {
+        ChiSquared cs = new ChiSquared();
+        chisquare = chiSquare(o);
+    }
+
+    private void calculateChiP() {
+        ChiSquared cs = new ChiSquared();
+        chiSquareP = chiSquareTest(o);
+    }
+
+    public double getChiSquare() { return chisquare; }
+
+    public double getChiSquareP() { return chiSquareP; }
 }
--- src/test/java/org/monarchinitiative/phcompare/stats/ChiSquaredTest.java
+++ src/test/java/org/monarchinitiative/phcompare/stats/ChiSquaredTest.java
@@ -1,6 +1,7 @@
 package org.monarchinitiative.phcompare.stats;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -9,8 +10,33 @@ import org.junit.Test;
 public class ChiSquaredTest {
 
 
-    @Test
-    public void testSanity() {
-        Assert.assertEquals(1,1);
+    long a[] = {7, 87, 12, 9};
+    long b[] = {1, 18, 3, 1};
+    long c[] = {3, 84, 4, 7};
+    long o[][] = {a, b, c};
+    //double e[] = {30, 60, 10};
+    static double epsilon = .0000000001;
+
+    @BeforeClass
+    public static void setup() throws Exception {
+
     }
+
+
+    @Test
+    public void testChiSquare() {
+        ChiSquared cs = new ChiSquared(o);
+        double expected = 5.488545890584;
+
+        Assert.assertEquals(expected,cs.getChiSquare(),epsilon);
+    }
+
+    @Test
+    public void testChiSquareP() {
+        ChiSquared cs = new ChiSquared(o);
+        double expected = 0.482842169465;
+
+        Assert.assertEquals(expected,cs.getChiSquareP(),epsilon);
+    }
+
 }
