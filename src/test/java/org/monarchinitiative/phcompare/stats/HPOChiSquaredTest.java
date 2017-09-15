fc79188b411fa1937b0ec4bc06ceb50ae4031169
Hannah Blau <hannah.blau@jax.org>
2017-09-15 15:25:43 -0400

    Created HPOChiSquaredTest, not yet finished.

---
+++ src/test/java/org/monarchinitiative/phcompare/stats/HPOChiSquaredTest.java
@@ -0,0 +1,29 @@
+package org.monarchinitiative.phcompare.stats;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Hannah Blau (blauh)
+ * @version 0.0.1
+ * @since 15 Sep 2017
+ */
+public class HPOChiSquaredTest {
+    @Test
+    public void compareTo() throws Exception {
+    }
+
+    @Test
+    public void getChiSquare() throws Exception {
+    }
+
+    @Test
+    public void getChiSquareP() throws Exception {
+    }
+
+    @Test
+    public void getHPOtermID() throws Exception {
+    }
+
+}
